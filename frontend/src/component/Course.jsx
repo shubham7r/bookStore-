b50ed934a6b76f885acb34a319fdd3c72b48ad67
shubham shelke <shubhamshelke779@gmail.com>
2025-06-19 16:39:53 +0530

    adding back button fuctionality

--- frontend/src/component/Course.jsx
+++ frontend/src/component/Course.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Cards from "./Cards";
 import list from "../data/list.json";
+import {Link} from "react-router-dom";
 
 function Course() {
   
@@ -22,9 +23,11 @@ function Course() {
             consequatur! whats
           </p>
 
-          <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
-            Back
-          </button>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
         </div>
         <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
           {list.map((item) => (
