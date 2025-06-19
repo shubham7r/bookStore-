fe0b8a28527a280cb77f5961d525c4934e6c4516
shubham shelke <shubhamshelke779@gmail.com>
2025-06-19 12:32:08 +0530

    delete public/list.json and change in component/Course.jsx file

--- frontend/src/component/Course.jsx
+++ frontend/src/component/Course.jsx
@@ -1,21 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Cards from "./Cards";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import list from "../data/list.json";
+
 function Course() {
-  const [book, setBook] = useState([]);
-  useEffect(() => {
-    const getBook = async () => {
-      try {
-        const res = await axios.get("http://localhost:4001/book");
-        console.log(res.data);
-        setBook(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getBook();
-  }, []);
+  
   return (
     <>
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
@@ -34,17 +22,13 @@ function Course() {
             consequatur! whats
 
           </p>
-          <Link to="/">
+         
             <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
               Back
             </button>
-          </Link>
+          
         </div>
-        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
-          {book.map((item) => (
-            <Cards key={item.id} item={item} />
-          ))}
-        </div>
+     
       </div>
     </>
   );
